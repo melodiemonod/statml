6e68500decba416c7745eb9581bdfe5951c1a5a0
Mélodie Monod <50656349+melodiemonod@users.noreply.github.com>
2019-10-25 15:05:17 +0200

    Add files via upload

--- Bayesian_statistics/PSIS.R
+++ Bayesian_statistics/PSIS.R
@@ -1,8 +1,9 @@
-library("mvtnorm")
-library(rstan)
+library("data.table") 
+library(rstan) ### stan is used for VI
 library(loo)  ### loo is used for PSIS
 
-# cluster
+
+# change 
 setwd("/rds/general/user/mm3218/home/projects/CDT_modules/bayesian_stats")
 indir = "/rds/general/user/mm3218/home/projects/CDT_modules/bayesian_stats"
 
@@ -10,7 +11,7 @@ rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
 
-## SIMULATED DATA
+## SIMULATE DATA
 
 n = 5; sigma = 1.2
 x = matrix(nrow = n, ncol = 2, c(rep(1, n), 0:(n-1)))
@@ -19,13 +20,14 @@ cov0 = diag(rep(10^2,2)); mean0 = cbind(rep(0,2))
 alpha = rnorm(1,0,10); beta = rnorm(1,0,10)
 y = cbind(rnorm(n, alpha + x[,2]*beta, sigma))
 
+
 ## EXACT POSTERIOR DISTRIBUTION
 
 cov_posterior = solve(solve(cov0) + t(x)%*%x/(sigma^2))
 mean_posterior = cov_posterior%*%(solve(cov0)%*%mean0 + t(x)%*%y/(sigma^2))
 
 
-## PRIORS
+## BAYESIAN LINEAR REGRESSION
 stan_code='
 data {
 int <lower=0> n;
@@ -46,7 +48,7 @@ beta ~ normal(mean_prior[2], sd_prior[2]);
 }
 generated quantities{
 real  log_density;
-log_density=normal_lpdf(y |alpha + x * beta, 1.2)+normal_lpdf(alpha|mean_prior[1], sd_prior[1])+normal_lpdf(beta|mean_prior[2], sd_prior[2]) ; //multi_normal_lpdf(beta|mean_prior,cov_prior);
+log_density=normal_lpdf(y |alpha + x * beta, 1.2)+normal_lpdf(alpha|mean_prior[1], sd_prior[1])+normal_lpdf(beta|mean_prior[2], sd_prior[2]) ; 
 }
 '
 
@@ -57,25 +59,24 @@ ip_weighted_average=function(lw, x){
 }
 
 
-## FIND k
-tol = 0.00001 # choose relative tolerance. The default in the published ADVI paper is 0.01.
-sim_N=100   # repeat simulations. 50 is enough as we can check the sd. The only uncertainty is stochastic optimization in ADVI
+## FIND k and PMSE
+tol = 0.00001 # choose relative tolerance for VI. The default in the published ADVI paper is 0.01.
+sim_N=100   # repeat simulations. 100 is enough as we can check the sd. The only uncertainty is stochastic optimization in ADVI
 I = 5 # true posterior and 4 priors
-# ceate a list with all the priors
+# ceate a list with the priors
 prior = list(prior1 = list(mean_prior = c(0,0), sd_prior = c(10,10)),
              prior2 = list(mean_prior = c(0,0), sd_prior = c(10, 2)), 
              prior3 = list(mean_prior = c(0,0), sd_prior = c(10, 100)),
              prior4 = list(mean_prior = c(0,100), sd_prior = c(10,10)))
 
-
-
-K_hat=matrix(NA,sim_N,I ); RMSE=EDS = matrix(NA,sim_N,(I-1) )
+K_hat=matrix(NA,sim_N,I); RMSE = matrix(NA,sim_N,(I-1))
 vi_parameter_mean = vi_parameter_sd = array(NA,c(sim_N,(I-1), 2))
+
 set.seed(1000)
 for(i in 1:I){
   for(sim_n in 1:sim_N){  
     if(i < I){
-      # VI
+      # Sample using VI
       fit_vb=vb(m, data=list(x=x[,2],y=as.vector(y), D=2,n=n, sd_prior = prior[[i]]$sd_prior, mean_prior = prior[[i]]$mean_prior), 
                 iter=5e5,output_samples=1e5,tol_rel_obj=tol,eta=0.05,adapt_engaged=F) 
       vb_samples=extract(fit_vb)
@@ -84,15 +85,16 @@ for(i in 1:I){
       vi_parameter_sd[sim_n,i,]=apply(trans_parameter, 2, sd)
       normal_likelihood=function(trans_parameter){
         one_data_normal_likelihood=function(vec){
-          return( sum( dnorm(vec,mean=vi_parameter_mean[sim_n,i,],sd=vi_parameter_sd[sim_n,i,],  log=T)))
+          return(sum(dnorm(vec,mean=vi_parameter_mean[sim_n,i,],sd=vi_parameter_sd[sim_n,i,],  log=T)))
         }
-        return( apply(trans_parameter, 1, one_data_normal_likelihood))
+        return(apply(trans_parameter, 1, one_data_normal_likelihood))
       }
       # compule log weights
       lp_vi=normal_likelihood(trans_parameter)
       lp_target=vb_samples$log_density
       
     } else{
+      # Sample using the true posterior
       trans_parameter=cbind(rnorm(1e5, mean_posterior[1], sqrt(cov_posterior[1,1])), rnorm(1e5, mean_posterior[2], sqrt(cov_posterior[2,2])))
       normal_likelihood_true=function(trans_parameter){
         one_data_normal_likelihood=function(vec){
@@ -112,22 +114,14 @@ for(i in 1:I){
       lp_target=normal_model(trans_parameter)
     }
     ip_ratio=lp_target-lp_vi
-
+    
     # fit weights to a Generalized Pareto with loo
     ok=complete.cases(ip_ratio)
     joint_diagnoistics=psis(log_ratios=as.vector(ip_ratio[ok]))
     K_hat[sim_n,i]=joint_diagnoistics$diagnostics$pareto_k
-  
+    
     if(i < I){
       RMSE[sim_n,i]=sqrt(mean((vi_parameter_mean[sim_n,i,2]-mean_posterior[2])^2))
-      EDS[sim_n,i]= vi_parameter_sd[sim_n,i,2]-sqrt(diag(cov_posterior))[2]
-      #psis_lw=joint_diagnoistics$lw_smooth
-      #bias_mean[1,sim_n,i,]=vi_parameter_mean-mean_posterior
-      #bias_square[1,sim_n,i,]=apply(trans_parameter^2, 2, mean)- diag(cov_posterior) + mean_posterior^2
-      #bias_mean[2,sim_n,i,]=ip_weighted_average(lw=ip_ratio, x=trans_parameter)-t(mean_posterior)
-      #bias_square[2,sim_n,i,]=ip_weighted_average(lw=ip_ratio, x=trans_parameter^2)-diag(cov_posterior) + t(mean_posterior^2)
-      #bias_mean[3,sim_n,i,]=ip_weighted_average(lw=psis_lw, x=trans_parameter)-t(mean_posterior)
-      #bias_square[3,sim_n,i,]=ip_weighted_average(lw=psis_lw, x=trans_parameter^2)-diag(cov_posterior) + t(mean_posterior^2)
     }
     
     print(paste("=======================    i=",i,"   ========================"))
@@ -137,17 +131,13 @@ for(i in 1:I){
 
 save(K_hat, RMSE, EDS,file=file.path(indir,"linear_model.RData"))
 
-stop()
 
 
 ## PLOTS
-# device
-indir = "~/Documents/PhD/Modules/Bayesian_Methods/Project/Code"
-system("scp /Users/test/Documents/PhD/Modules/Bayesian_Methods/Project/Code/PSIS_v2.R mm3218@login.cx1.hpc.ic.ac.uk:/rds/general/user/mm3218/home/projects/CDT_modules/bayesian_stats")
-system("scp mm3218@login.cx1.hpc.ic.ac.uk:/rds/general/user/mm3218/home/projects/CDT_modules/bayesian_stats/linear_model.RData /Users/test/Documents/PhD/Modules/Bayesian_Methods/Project/Code")
+
 load(file.path(indir,"linear_model.RData"))
 
-## k against prior ##
+## khat against prior ##
 y.median = apply(K_hat, 2, median)
 y.PI 	<- apply(K_hat, 2, function(x) quantile(x, prob=c(0.025,0.975)))
 y.PI	<- rbind(y.median, y.PI)
@@ -165,6 +155,7 @@ ggplot(y.PI[1:4,], aes(x=1:4, ymin=predicted_l95, ymax=predicted_u95)) +
 ggsave("CI_k.png", w = 5, h = 6, device = "png", path = file.path(indir, "figures"))
 
 
+## khat against RMSE ##
 df = data.frame(RMSE = as.vector(RMSE), K_hat = as.vector(K_hat[,1:(I-1)]), Prior = as.vector(sapply(1:(I-1), function(x) rep(x, sim_N))), EDS = as.vector(EDS))
 df$Prior = as.factor(df$Prior)
 ggplot(df, aes(x = K_hat, y = RMSE, color = Prior)) +
@@ -175,16 +166,8 @@ ggplot(df, aes(x = K_hat, y = RMSE, color = Prior)) +
   theme(text = element_text(size=25))
 ggsave("RMSE_k.png", w = 7, h = 6, device = "png", path = file.path(indir, "figures"))
 
-ggplot(df, aes(x = K_hat, y = EDS, color = Prior)) +
-  geom_line()+
-  geom_smooth()+
-  xlab(expression(hat(k))) +
-  ylab("EDS")+
-  theme_bw()+
-  theme(text = element_text(size=18))
 
-
-library("data.table")
+## True posterior compared to VI posteriors 
 x_beta = seq(10.5,14,0.01); x_alpha = seq(-2,7,length.out = length(x_beta))
 dff = data.frame(mean_all_alpha = c(mean_posterior[1], apply(vi_parameter_mean[,,1], 2, median)), mean_all_beta = c(mean_posterior[2], apply(vi_parameter_mean[,,2], 2, median)), 
                  sd_all_alpha = c(sqrt(diag(cov_posterior))[1], apply(vi_parameter_sd[,,1], 2, median)), sd_all_beta = c(sqrt(diag(cov_posterior))[2], apply(vi_parameter_sd[,,2], 2, median)), 
@@ -195,6 +178,7 @@ dff = as.data.table(dff)
 dff[, den_posterior_alpha := dnorm(x_alpha, mean_all_alpha, sd_all_alpha)]
 dff[, den_posterior_beta := dnorm(x_beta, mean_all_beta, sd_all_beta)]
 
+# beta
 ggplot(dff, aes(x = x_beta, y = den_posterior_beta, color = Posterior, linetype = Posterior)) +
   geom_line(size = 1)+
   xlab(expression(beta)) +
@@ -203,11 +187,10 @@ ggplot(dff, aes(x = x_beta, y = den_posterior_beta, color = Posterior, linetype 
   theme(text = element_text(size=25))
 ggsave("VI_density.png", w = 8, h = 6, device = "png", path = file.path(indir, "figures"))
 
+# alpha
 ggplot(dff, aes(x = x_alpha, y = den_posterior_alpha, color = Distribution)) +
   geom_line()+
   xlab(expression(alpha)) +
   ylab(expression(hat(pi(alpha))))+
   theme_bw()+
   theme(text = element_text(size=18))
-
-
