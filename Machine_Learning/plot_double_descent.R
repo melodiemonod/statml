f7d87842a9d210613f4cab45db20a7a633b5714c
Mélodie Monod <50656349+melodiemonod@users.noreply.github.com>
2019-11-21 14:58:55 +0000

    Add files via upload

--- Machine_Learning/plot_double_descent.R
+++ Machine_Learning/plot_double_descent.R
@@ -21,24 +21,24 @@ D_grid = c(seq(2, 900, 100), c(1, 2,4,5,7,10,13,15,20)*10^3)
 tmp = data.frame(method = c(rep("LS", length(D_grid)), rep(paste("Ridge lambda =", round(lambdas, digits = 4)), length(D_grid)) , rep(paste("Lasso lambda =", round(lambdas, digits = 4)), length(D_grid))), 
                  D = c(D_grid, rep(as.vector(sapply(D_grid, function(x) rep(x, length(lambdas)))), 2)),
                  MSE_training_i  = c(df[grepl( "ls.MSE_train", names(df))], df[grepl( "ridge.MSE_train", names(df))], df[grepl( "lasso.MSE_train", names(df))]),
-                 MSE_test_i  = log(c(df[grepl( "ls.MSE_test", names(df))], df[grepl( "ridge.MSE_test", names(df))], df[grepl( "lasso.MSE_test", names(df))])),
+                 MSE_test_i  = c(df[grepl( "ls.MSE_test", names(df))], df[grepl( "ridge.MSE_test", names(df))], df[grepl( "lasso.MSE_test", names(df))]),
                  MSE_training_l  = c(df2[grepl( "ls.MSE_train", names(df2))], df2[grepl( "ridge.MSE_train", names(df2))], df2[grepl( "lasso.MSE_train", names(df2))]),
-                 MSE_test_l  = log(c(df2[grepl( "ls.MSE_test", names(df2))], df2[grepl( "ridge.MSE_test", names(df2))], df2[grepl( "lasso.MSE_test", names(df2))])),
+                 MSE_test_l  = c(df2[grepl( "ls.MSE_test", names(df2))], df2[grepl( "ridge.MSE_test", names(df2))], df2[grepl( "lasso.MSE_test", names(df2))]),
                  ZOL_training_i  = c(df[grepl( "ls.ZOL_train", names(df))], df[grepl( "ridge.ZOL_train", names(df))], df[grepl( "lasso.ZOL_train", names(df))]),
                  ZOL_test_i  = c(df[grepl( "ls.ZOL_test", names(df))], df[grepl( "ridge.ZOL_test", names(df))], df[grepl( "lasso.ZOL_test", names(df))]),
                  ZOL_training_l  = c(df2[grepl( "ls.ZOL_train", names(df2))], df2[grepl( "ridge.ZOL_train", names(df2))], df2[grepl( "lasso.ZOL_train", names(df2))]),
                  ZOL_test_l  = c(df2[grepl( "ls.MSE_test", names(df2))], df2[grepl( "ridge.ZOL_test", names(df2))], df2[grepl( "lasso.ZOL_test", names(df2))]),
-                 L2_norm_i = log(c(df[grepl( "ls.L2_norm", names(df))], df[grepl( "ridge.L2_norm", names(df))], df[grepl( "lasso.L2_norm", names(df))])), 
-                 L2_norm_l = log(c(df2[grepl( "ls.L2_norm", names(df2))], df2[grepl( "ridge.L2_norm", names(df2))], df2[grepl( "lasso.L2_norm", names(df2))])))
+                 L2_norm_i = c(df[grepl( "ls.L2_norm", names(df))], df[grepl( "ridge.L2_norm", names(df))], df[grepl( "lasso.L2_norm", names(df))]), 
+                 L2_norm_l = c(df2[grepl( "ls.L2_norm", names(df2))], df2[grepl( "ridge.L2_norm", names(df2))], df2[grepl( "lasso.L2_norm", names(df2))]))
 
 D_grid = c(seq(2, 900, 50), c(1, 3, 6, 9, 10, 11, 13, 15, 20)*10^3)
 tmp2 = data.frame(method = c(rep("LS", length(D_grid))), 
                   D = c(D_grid), 
                   MSE_training  = c(df3[grepl( "ls.MSE_train", names(df3))]),
-                  MSE_test  = log(c(df3[grepl( "ls.MSE_test", names(df3))])),
+                  MSE_test  = c(df3[grepl( "ls.MSE_test", names(df3))]),
                   ZOL_training  = c(df3[grepl( "ls.ZOL_train", names(df3))]),
                   ZOL_test  = c(df3[grepl( "ls.ZOL_test", names(df3))]),
-                  L2_norm = log(c(df3[grepl( "ls.L2_norm", names(df3))])))
+                  L2_norm = c(df3[grepl( "ls.L2_norm", names(df3))]))
 
 
 p1.lasso = ggplot(tmp[grepl("Lasso", tmp$method),], aes(x = D, color = factor(method, levels = paste("Lasso lambda =", round(lambdas, digits = 4))))) +
@@ -102,31 +102,31 @@ ggsave(plot = plot_ridge, w = 7.5, h = 12, device = "pdf", filename  = file.path
 
 
 p1.ls = ggplot(tmp2[grepl("LS", tmp2$method),], aes(x = D, color = method)) +
-  geom_line(aes(y = MSE_test)) +
-  geom_point(aes(y = MSE_test), shape = 4) +
+  geom_line(aes(y = log(MSE_test))) +
+  geom_point(aes(y = log(MSE_test)), shape = 4) +
   labs(title = "Least norm - Analytical solution") +
   theme_bw() +
   ylab("Mean squared Error Test") +
   theme(axis.title.x=element_blank(), legend.position = "none")
 
 p2.ls = ggplot(tmp[grepl("LS", tmp$method),], aes(x = D, color = method)) +
-  geom_line(aes(y = MSE_test_i)) +
-  geom_point(aes(y = MSE_test_i), shape = 4) +
+  geom_line(aes(y = log(MSE_test_i))) +
+  geom_point(aes(y = log(MSE_test_i)), shape = 4) +
   labs(title = expression("Least norm - Ridge approximation", lambda, " = 1e-20")) +
   theme_bw() +
   ylab("Mean squared Error Test") +
   theme(axis.title.x=element_blank(), legend.position = "none")
 
 p3.ls =  ggplot(tmp2[grepl("LS", tmp2$method),], aes(x = D, color = method)) +
-  geom_line(aes(y = L2_norm))+
-  geom_point(aes(y = L2_norm), shape = 4) +
+  geom_line(aes(y = log(L2_norm)))+
+  geom_point(aes(y = log(L2_norm)), shape = 4) +
   theme_bw()+
   ylab("Norm") +
   theme(axis.title.x=element_blank(), legend.position = "none")
 
 p4.ls =  ggplot(tmp[grepl("LS", tmp$method),], aes(x = D, color = method)) +
-  geom_line(aes(y = L2_norm_i))+
-  geom_point(aes(y = L2_norm_i), shape = 4) +
+  geom_line(aes(y = log(L2_norm_i)))+
+  geom_point(aes(y = log(L2_norm_i)), shape = 4) +
   theme_bw()+
   ylab("Norm") +
   theme(axis.title.x=element_blank(), legend.position = "none")
@@ -148,4 +148,4 @@ p6.ls = ggplot(tmp[grepl("LS", tmp$method),], aes(x = D, color = method)) +
   theme(legend.position = "none")
 
 plot_ls = grid.arrange(p1.ls, p2.ls, p3.ls, p4.ls, p5.ls, p6.ls, nrow = 3, ncol =2, heights = c(4, 3, 3))
-ggsave(plot = plot_ls, w = 6, h = 10, device = "pdf", filename  = file.path(indir,"plot_leastnorm.pdf"))
+ggsave(plot = plot_ls, w = 10, h = 10, device = "pdf", filename  = file.path(indir,"plot_leastnorm.pdf"))
